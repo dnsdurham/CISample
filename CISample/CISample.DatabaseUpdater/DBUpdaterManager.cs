bbf678f3bfa794d91951945c92689b357016c358
Doug Durham <github@dnsdurham.com>
2014-02-18 07:14:33 -0600

    Added migration folders and manager class

---
+++ CISample/CISample.DatabaseUpdater/DBUpdaterManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CISample.DatabaseUpdater
+{
+    public interface IDBUpdaterManager
+    {
+        void RunMigration(string databaseName);
+    }
+
+    class DBUpdaterManager : IDBUpdaterManager
+    {
+        public void RunMigration(string databaseName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
